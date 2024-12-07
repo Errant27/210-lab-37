402b49e25a99dbae7479487e2a2e58eeab0ad7f2
Errant27 <temporal.cerauno@gmail.com>
2024-12-06 18:50:13 -0800

    commit 1 (part 1)

--- 210-lab-37/main.cpp
+++ 210-lab-37/main.cpp
@@ -1,14 +1,24 @@
-//
-//  main.cpp
-//  210-lab-37
-//
-//  Created by Neil Orton on 12/6/24.
-//
-
+// 210 | Lab 37 | Neil Orton
+// IDE used: Xcode
+// Part 1
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void sum_ascii(string);    // sum_ascii function prototype
+
+int main() {
+
+    sum_ascii("Defenestratee");    // sum_ascii function call, takes string as an argument
+
     return 0;
+}
+
+void sum_ascii(string test) {    // sum_ascii function definition
+    int test_sum = 0;
+    
+    for (int i = 0; i < test.size(); i++)    // string elements (characters) are summed in the loop
+        test_sum += (int) test.at(i);
+    
+    cout << test_sum << endl;    // sum of the string's ascii values is outputted
+    
 }
